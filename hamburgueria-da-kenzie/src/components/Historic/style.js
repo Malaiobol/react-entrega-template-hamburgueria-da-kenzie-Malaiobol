97b550e743f8ab0c7429ae96c1ea8870a3ba741b
Malaiobol <wow_ju1@hotmail.com>
2022-11-24 22:54:39 -0300

    feat:initial-design

---
+++ hamburgueria-da-kenzie/src/components/Historic/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledHistoric = styled.div`
+  h2 {
+    color: var(--colorGrey100);
+
+    font-weight: 700;
+    font-size: 1.625rem;
+    line-height: 34px;
+  }
+
+  span {
+    color: var(--colorGrey20);
+
+    font-weight: 700;
+    font-size: 1.625rem;
+    line-height: 34px;
+  }
+`;
